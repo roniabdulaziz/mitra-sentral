8da863b3100e4aed23dd5a12aa5fd5a22068f774
Roni <164780266+roniabdulaziz@users.noreply.github.com>
2025-12-30 20:17:20 +0800

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,11 @@
+console.log("auth.js aktif");
 
+document.getElementById("btnLogin").addEventListener("click", function () {
+  console.log("Tombol login diklik");
+
+  const hp = document.getElementById("hp").value;
+  const pin = document.getElementById("pin").value;
+
+  console.log("HP:", hp);
+  console.log("PIN:", pin);
+});
